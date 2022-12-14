3fc2bea13ff817e7e50e55da30037a474f0e1f09
Ray <i@r-ay.cn>
2022-12-14 15:28:57 +0800

    2-12

---
+++ pages/2-12/2-12.js
@@ -0,0 +1,40 @@
+// pages/2-12/2-12.js
+function createRand() {
+  let rand = []
+  let sum = 0
+  for (let i = 0; i < 6; i++) {
+    let r = (Math.random() * 100).toFixed(2) * 1
+    rand.push(r)
+    sum += r
+  }
+  let rtn = {}
+  rtn.rand = rand
+  rtn.sum = sum
+  return (rtn)
+}
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    rand: [],
+    sum: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.newRand()
+  },
+
+  newRand: function () {
+    let newData = createRand()
+    this.setData({
+      rand: newData.rand,
+      sum: newData.sum
+    })
+  }
+})
