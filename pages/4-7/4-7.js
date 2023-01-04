ad800b56becb831800c7dabe59f0beb0e3651f6c
Ray <i@r-ay.cn>
2023-01-04 14:59:14 +0800

    4-7

---
+++ pages/4-7/4-7.js
@@ -0,0 +1,9 @@
+// pages/4-7/4-7.js
+Page({
+
+  data: {
+    imgSrc: 'https://bingpic.wd-api.com/latest.jpeg',
+    imgModes: ['aspectFit', 'scaleToFill', 'aspectFill', 'top', 'bottom', 'center', 'left', 'right', 'top left', 'top right', 'bottom left', 'bottom right']
+  },
+
+})
